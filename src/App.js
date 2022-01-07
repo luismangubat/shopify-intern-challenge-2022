ad3895753efb32e366f4a5a90cb9fb50bbede037
luismangubat <joseluismangubat@gmail.com>
2022-01-07 09:07:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+const API_KEY = "https://api.nasa.gov/planetary/apod?api_key=g9M8wUGMcefg71f0dj1NmB4LblvvgSwFPv6BVZPa";
+
 
 function App() {
+
+  const [photoData, setPhotodata] = useState("");
+  const [searchVal, setSearchVal] = useState("");
+
+  const getPhotoData = async (searchVal) => {
+    try {
+      const url = "https://api.nasa.gov/planetary/apod?api_key=g9M8wUGMcefg71f0dj1NmB4LblvvgSwFPv6BVZPa";
+      const response =  await fetch(url);
+      const responseJson = await response.json();
+      console.log(response)
+      
+      if (responseJson.Search) {
+        setMovies(responseJson.Search);
+      } 
+        return []
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+
+
   return (
     <div className="App">
       <header className="App-header">
